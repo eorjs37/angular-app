3079a37dc6cc2bb2907ae75aa721310f8f9da838
CHOI DAE GEON <chleorjs37@gmail.com>
2021-09-29 07:32:57 +0900

    히어로 서비스 수정

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,7 +16,8 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]>{
     return this.http.get<Hero[]>(this.heroesUrl)
-      .pipe(tap(_ => this.log('fetched heroes')),
+      .pipe(
+            tap(_ => this.log('fetched heroes')),
             catchError(this.handleError<Hero[]>('getHeroes', []))
       );
   }
@@ -54,6 +55,32 @@ export class HeroService {
     return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
       tap(_ => this.log(`updated hero id=${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
+    )
+  }
+
+  addHero(hero: Hero) {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`add hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    )
+  }
+
+  deleteHero(id: number): Observable<Hero>{
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`),
+      catchError(this.handleError<Hero>('deleteHero')))
+    )
+  }
+
+  searchHeroes(term: string): Observable<Hero[]>{
+    if (!term.trim()) {
+      return of([]);
+    }
+
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ? this.log(`found heroes matching "${term}"`) : this.log(`no heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
     )
   }
 }
