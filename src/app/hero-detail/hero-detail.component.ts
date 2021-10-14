cd65e6314d08867441b545debee8b77ff5b2e358
CHOI DAE GEON <chleorjs37@gmail.com>
2021-10-14 23:41:19 +0900

    detail 추가

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -13,6 +13,22 @@ export class HeroDetailComponent implements OnInit {
   constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) { }
 
   ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void{
+    this.location.back();
+  }
+
+  save(): void{
+    if (this.hero) {
+      this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+    }
+  }
 }
