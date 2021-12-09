42806d930f523d53df042c0b5c5311000dc5b69b
CHOI DAE GEON <chleorjs37@gmail.com>
2021-12-09 19:48:25 +0900

    라우팅 추가

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { FirstComponent } from './first/first.component';
-import { SecondComponent } from './second/second.component';
+import { AddHeroComponent } from './add-hero/add-hero.component';
+import { NewHeroesComponent } from './new-heroes/new-heroes.component';
+import { CoffeeMenuComponent } from './coffee-menu/coffee-menu.component';
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/coffee-menu', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'first', component: FirstComponent },
-  { path: 'second', component: SecondComponent },
+  { path: 'add', component: AddHeroComponent },
+  { path: 'new-heroes', component: NewHeroesComponent },
+  { path: 'coffee-menu', component: CoffeeMenuComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,58 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule} from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './service/in-memory-data.service';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms'; 
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddHeroComponent } from './add-hero/add-hero.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { NewHeroesModule } from './new-heroes/new-heroes.module';
+import { CoffeeMenuModule } from './coffee-menu/coffee-menu.module';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    AddHeroComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot({}, {
+      runtimeChecks: {
+        // strictStateSerializability: true,
+        // strictActionSerializability: true,
+        // strictActionWithinNgZone: true,
+        // strictActionTypeUniqueness: true,
+        // strictStateImmutability: false, 
+        // strictActionImmutability: false,
+      }
+    }),
+    NewHeroesModule,
+    CoffeeMenuModule,
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+    }),
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    ),
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
