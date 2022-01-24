be1243bb544e4be785df0801728a98727f8ac81f
chleorjs <chleorjs37@gmail.com>
2022-01-24 22:03:37 +0900

    Rxjs import

--- src/app/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs.component.ts
@@ -1,20 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-import { fromEvent } from 'rxjs';
-import {scan} from 'rxjs/operators'
+import { FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Validators } from '@angular/forms';
+// RxJS 임포트
+import { Subscription, Observable, of, throwError } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs/operators';
+
+interface GithubUser {
+  login: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
   styleUrls: ['./rxjs.component.css']
 })
 export class RxjsComponent implements OnInit {
+  searchInput: FormControl = new FormControl('');
+  email: FormControl = new FormControl('',[Validators.required, Validators.email]);
+  githubUser: GithubUser;
+  subscription: Subscription;
+
+  constructor(private http: HttpClient) { 
+    this.email.valueChanges
+      .pipe(
+        debounceTime(1000),
+        distinctUntilChanged(),
+        filter(() => this.email.valid),
+        switchMap((x) => this.test(x))
+      )
+      .subscribe((x) => {
+        if (this.email.valid) {
+          console.log('x : ',x);
+        }
+    })
+  }
+
+  ngOnInit() {
+    this.test('eorjs37');
+    
+  }
+
   
-  constructor() { 
-    const observable = fromEvent(document, 'click').pipe(scan(count => count + 1, 0))
-    const observer = (count: number) => console.log(`Clicked ${count} times`);
-    observable.subscribe(observer);
+  getGithubUser(userId: string){
   }
 
-  ngOnInit(): void {
+  test(userId: string): Observable<any> {
+    return this.http.get(`https://api.github.com/users/${userId}`)
   }
-
 }
