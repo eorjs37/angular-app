6e422f66aa8cf78352be6910a216ce08d7fbef88
chleorjs <chleorjs37@gmail.com>
2022-02-05 09:02:37 +0900

    타이머 서비스 추가

--- src/app/service/timer.service.ts
+++ src/app/service/timer.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Observable, timer } from 'rxjs';
+import { Observable, Subject, timer } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class TimerService {
-  private isStop: boolean = false;
-  private timer: any;
-  constructor() { }
+  private $pause = new Subject();
 
-  timerStartCounter() : Observable<number>{
-    return timer(0, 1000)
+  constructor() { 
   }
 
-  timerStopCounter(timerObject: any) {
-    timerObject.unsubscribe();
+
+
+  timerStartCounter() : Observable<number>{
+    return timer(0, 1000).pipe(
+      takeUntil(this.$pause)
+    )
+  }
+
+  timerStopCounter() {
+    this.$pause.next();
   }
 }
