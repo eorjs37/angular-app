5090ef62a483542d3e82b777931472a5090133f5
chleorjs <chleorjs37@gmail.com>
2022-02-16 21:20:31 +0900

    app component 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { interval, Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,6 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Let`s Study Angular';
+  nowdate$: Observable<number>;
+  
   constructor() {
+    this.nowdate$ = interval(1000).pipe(
+      map(x => x = new Date().getTime())
+    );
   }
 }
