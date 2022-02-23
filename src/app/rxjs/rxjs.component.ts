391504f33abca99c43705e4a4f1c0a6ab50351c2
chleorjs <chleorjs37@gmail.com>
2022-02-23 21:06:27 +0900

    rxjs 추가

--- src/app/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs.component.ts
@@ -1,52 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { Validators } from '@angular/forms';
 // RxJS 임포트
-import { Subscription, Observable, of, throwError } from 'rxjs';
-import { debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs/operators';
-
-interface GithubUser {
-  login: number;
-  name: string;
-}
-
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-rxjs',
   templateUrl: './rxjs.component.html',
   styleUrls: ['./rxjs.component.css']
 })
 export class RxjsComponent implements OnInit {
-  searchInput: FormControl = new FormControl('');
-  email: FormControl = new FormControl('',[Validators.required, Validators.email]);
-  githubUser: GithubUser;
-  subscription: Subscription;
 
   constructor(private http: HttpClient) { 
-    this.email.valueChanges
-      .pipe(
-        debounceTime(1000),
-        distinctUntilChanged(),
-        filter(() => this.email.valid),
-        switchMap((x) => this.test(x))
-      )
-      .subscribe((x) => {
-        if (this.email.valid) {
-          console.log('x : ',x);
-        }
-    })
-  }
-
-  ngOnInit() {
-    this.test('eorjs37');
     
   }
 
-  
-  getGithubUser(userId: string){
+  ngOnInit() {
+    this.makeObserable();
   }
 
-  test(userId: string): Observable<any> {
-    return this.http.get(`https://api.github.com/users/${userId}`)
+  makeObserable() {
+    //1.옵저버블이 구독될때 호출 되는 함수
+    const subscriber = (observer: any) => {
+      try {
+        //next 함수를 통해 데이터 방출
+        observer.next(1);
+        observer.next(2);
+
+        //complete 함수를 호출
+        observer.complete();
+      } catch (error) {
+        observer.error(error);
+      }
+      finally {
+        // 구독 해지될 때 호출되는 콜백 함수
+        return () => console.log('Unsubscribed!')
+      }
+    }
+
+    //2.옵져버블 생성
+    const observer$ = new Observable(subscriber);
+
+
+    //3.구독(subscription)
+    observer$.subscribe(
+      // 옵저버블이 방출한 next 노티피케이션에 반응하는 next 메소드
+      value => console.log(value),
+      // 옵저버블이 방출한 error 노티피케이션에 반응하는 error 메소드
+      error => console.error(error),
+      // 옵저버블이 방출한 complete 노티피케이션에 반응하는 complete 메소드
+      () => console.log('Complete')
+    )
+
   }
+
 }
