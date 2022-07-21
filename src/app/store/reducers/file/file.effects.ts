084c77c37a2219114244e49dfca743061dd686af
CHOI DAE GEON <chleorjs37@gmail.com>
2022-07-21 20:39:48 +0900

    커밋

--- src/app/store/reducers/file/file.effects.ts
+++ src/app/store/reducers/file/file.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { act, Actions, createEffect,ofType,concatLatestFrom  } from '@ngrx/effects';
-import { map, mergeMap, withLatestFrom,tap, filter, concatMap } from 'rxjs/operators';
+import { map, mergeMap, withLatestFrom,tap, filter, concatMap, switchMap, catchError } from 'rxjs/operators';
 import { HttpEvent } from '@angular/common/http';
 import { addBeforeFileAction,addFileSuccess, beforeSendArrayFile } from './file.actions';
 import { FileuploadService } from 'src/app/service/fileupload.service';
@@ -27,11 +27,15 @@ export class FileEffects{
         ()=> this.actions$.pipe(
             ofType(beforeSendArrayFile),
             concatLatestFrom(() => this.store$.select(getFileList)),
-            map(([_,actions])=> from(actions).pipe(
-                concatMap(item => this.apiService.fileUplaod(item).pipe(
-                    map((res:HttpEvent<any>) => addFileSuccess({res}))
-                ))
-            ).subscribe),
+            map(([_,actions])=> {
+                from(actions).pipe(
+                    concatMap(item => this.apiService.fileUplaod(item).pipe(
+                        map((res:HttpEvent<any>) => addFileSuccess({res})),
+                        // catchError((err) => console.error('err : ',err))
+                    ))
+                ).subscribe()
+            }),
+
            
         ),
         { dispatch: false }
